5b64084eed38ecb1743ef122bc026630881cf51e
manny-uncharted <omoebun52@gmail.com>
2022-10-23 00:48:54 +0100

    changed db to postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'password',
     }
 }
 
